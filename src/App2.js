09366e65b3d2a619c560a0591224bdfa0a9d799c
Uriel Badillo <ubadillog1600@alumno.ipn.mx>
2022-10-22 21:34:42 -0500

    “added”

---
+++ src/App2.js
@@ -0,0 +1,30 @@
+import './styles/styleGrid.css';
+import AppNav from './components/AppNav';
+import ComplexGrid from './components/main'
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import IconButton from '@mui/material/IconButton';
+import DataTable from './components/table';
+function App() {
+  return (
+  <Box className='container' id= 'inicio'>
+      <Grid className='item item1'>
+        <AppNav />
+      </Grid>
+      <Grid className='item item2'>
+        <ComplexGrid />
+      </Grid>
+      <Grid className='item item3'>
+      <IconButton className='progress' href= '#item4'>
+        <ArrowDownwardIcon />
+      </IconButton>
+      </Grid>
+      <Grid className='item4' id= 'item4'>
+       <DataTable/>
+      </Grid>
+  </Box>
+  );
+}
+
+export default App;
